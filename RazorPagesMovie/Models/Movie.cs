14c2c52ded4c04c0d4b02ecdc498274a106045fb
klapouchy86 <157887112+klapouchy86@users.noreply.github.com>
2024-02-04 16:10:29 +0100

    Time removed from addMovie

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,11 +1,18 @@
-﻿namespace RazorPagesMovie.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
 {
     public class Movie
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
